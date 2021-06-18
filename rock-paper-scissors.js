d50655ce054c91823f110661ba234b86dd3629b6
SyntaxV8 <yordi.vt@hotmail.com>
2021-06-18 14:21:20 +0200

    finished base game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,35 @@
-function game() {
+(function game () {
 
-}(game());
+    const playerChoices = ['rock', 'paper', 'scissors'];
+    let playerChoice;
+    let computerChoice;
+
+    document.getElementById("rock").addEventListener('click', function () {
+        playerChoice = playerChoices[0]
+    });
+    document.getElementById("paper").addEventListener('click', function () {
+        playerChoice = playerChoices[1]
+    });
+    document.getElementById("scissors").addEventListener('click', function () {
+        playerChoice = playerChoices[2]
+    });
+
+    document.getElementById("play").addEventListener('click', function () {
+
+       computerChoice = playerChoices[Math.floor(Math.random()*playerChoices.length)];
+
+       if (playerChoice === computerChoice) {
+           document.getElementById('result').innerHTML = `It's a tie! The computer took ${computerChoice}.`
+       } else {
+           if (playerChoice === playerChoices[0] && computerChoice === playerChoices[2]
+               || playerChoice === playerChoices[1] && computerChoice === playerChoices[0]
+               || playerChoice === playerChoices[2] && computerChoice === playerChoices[1]) {
+               document.getElementById("result").innerHTML = `Congratulations! You won! The computer took ${computerChoice}.`
+           } else {document.getElementById("result").innerHTML = `Too bad you lose! The computer took ${computerChoice}.`}
+       }
+
+    });
+
+
+})()
+
